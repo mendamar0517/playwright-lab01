d3aeb5b471f6c58faa3b026ce52b3c06304cc92b
mendamar0517 <mend2023@gmail.com>
2026-09-06 14:08:24 +0800

    Add failed login test

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -12,3 +12,17 @@ test("Амжилттай нэвтрэх", async ({ page }) => {
   await page.getByRole("button", { name: "Open Menu" }).click();
   await page.getByText("Logout").click();
 });
+
+test("Буруу нууц үгээр нэвтрэх", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill("wrong_password");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(
+    page.getByText(
+      "Username and password do not match any user in this service",
+    ),
+  ).toBeVisible();
+});
