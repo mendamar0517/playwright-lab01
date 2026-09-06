59442831f0bd2a5c58453123e7fee2e01fadca76
mendamar0517 <mend2023@gmail.com>
2026-09-06 14:03:54 +0800

    Add successful login test

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from "@playwright/test";
+
+test("Амжилттай нэвтрэх", async ({ page }) => {
+  await page.goto("https://www.saucedemo.com/");
+
+  await page.getByPlaceholder("Username").fill("standard_user");
+  await page.getByPlaceholder("Password").fill("secret_sauce");
+  await page.getByRole("button", { name: "Login" }).click();
+
+  await expect(page.getByText("Products")).toBeVisible();
+
+  await page.getByRole("button", { name: "Open Menu" }).click();
+  await page.getByText("Logout").click();
+});
